e7a07dc035070096d54b21f811e3182d1d26e9c1
Lokesh Ojha <66007061+lokeshojha@users.noreply.github.com>
2021-11-17 12:11:38 +0530

    Add files via upload

---
+++ Codes (Leetcode) + (450DSA)/LEETCODE 268. Missing Number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum=n*(n+1)/2;
+        int temp=0;
+        int sum2=accumulate(nums.begin(), nums.end(),temp);
+        return sum-sum2;
+    }
+};
+/*
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum=n*(n+1)/2;
+        int temp=0;
+        int sum2=accumulate(nums.begin(), nums.end(),temp);
+        return sum-sum2;
+    }
+};
+
+*/
